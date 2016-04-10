e8540f568141b3e41ab11c10503f0c2edd345c0f
shharn <shharn8389@naver.com>
2016-04-10 16:45:35 +0900

    Create Couting Sheep.cpp

---
+++ Algorithm-Problem/Google-Codejam/Counting-Sheep/Couting Sheep.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <set>
+#define INPUT_PATH "A-large-practice.in"
+#define OUTPUT_PATH "A-large-practice-out.out"
+using namespace std;
+set<char> elements;
+
+void mark(int N) {
+	string each_digits = std::to_string(N);
+	for (int i = 0; i < each_digits.size(); i++)
+		elements.insert(each_digits.at(i));
+}
+
+bool checkAllMembers() {
+	return elements.size() == 10;
+}
+
+void main() {
+	freopen(INPUT_PATH, "r", stdin);
+	freopen(OUTPUT_PATH, "w", stdout);
+	int T, currCase;
+	cin >> T;
+	for (currCase = 1; currCase <= T; currCase++) {
+		elements.clear();
+		int N, firstNumber;
+		cin >> N;
+		firstNumber = N;
+		while (!checkAllMembers() && N != 0) {
+			mark(N);
+			N += firstNumber;
+		}
+		cout << "Case #" << currCase << ": ";
+		if (checkAllMembers())
+			cout << N - firstNumber;
+		else
+			cout << "INSOMNIA";
+		cout << endl;
+	}
+	_fcloseall();
+}
