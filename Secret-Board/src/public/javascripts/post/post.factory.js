eb7b49e94ede8688f82113ddeaeae84a054dec57
shharn <shharn8389@naver.com>
2016-06-30 20:16:35 +0900

    Update post.factory.js

--- Secret-Board/src/public/javascripts/post/post.factory.js
+++ Secret-Board/src/public/javascripts/post/post.factory.js
@@ -2,29 +2,26 @@ angular
 	.module('postModule')
 		.factory('post', ['$http', 'auth', function($http, auth){
 			var obj = {
-					posts: []
+					user : {},
 			};
 			
 			obj.getPosts = function(userid){
 				return $http.get('/' + userid + '/posts', 
 						{headers: {Authorization: 'Bearer ' + auth.getToken()}})
 						.success(function(data){
-							console.log("In success");
-							angular.copy(data.posts, obj.posts);
-							obj.totalPosts = data.totalPosts;
+							angular.copy(data, obj.user);
 				});
 			};
 			
 			obj.addPost = function(title, body){
-				var data;
-				var d = new Date();
+				var data = {};
 				data.title = title;
 				data.body = body;
-				data.date = d.now();
-				return $http.post('/' + obj.userid + '/posts/newpost', data, 
+				data.date = Date.now();
+				return $http.post('/' + obj.user._id + '/posts/newpost', data, 
 						{ headers: {Authorization: 'Bearer ' + auth.getToken()}})
-						.success(function(res){
-							obj.posts.push(data);
+						.success(function(data){
+							obj.user = data;
 						});
 			};
 			
