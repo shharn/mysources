fe51c62381cb95532cdd97878638c579476a151a
shharn <shharn8389@naver.com>
2016-07-07 21:29:09 +0900

    Update new-post.controller.js

--- Secret-Board/src/public/javascripts/new-page/new-post.controller.js
+++ Secret-Board/src/public/javascripts/new-page/new-post.controller.js
@@ -1,16 +1,26 @@
 angular
 	.module('newpostModule')
 		.controller('newpostController', 
-				['$scope', '$state', 'post', function($scope, $state, post){
-					$scope.post = {};
+				['$scope', '$state', '$stateParams', '$location', 'post', 
+				 function($scope, $state, $stateParams, $location, post){
+					$scope.isEdit = $location.path().split("/")[4];
+					$scope.post =  $scope.isEdit? post.user.posts[$stateParams.postIndex] : {};							
 					$scope.submitNewPost = function(){
-						post.addPost($scope.post.title, $scope.post.body)
-						.success(function(){
-							$state.go('userhome', {userid:post.obj.userid});
-						});
+						if($scope.isEdit){
+							post.editPost($scope.post.title, $scope.post.body, $scope.post._id)
+							.then(function(){
+								$state.go('userhome', {userid:post.user._id, pagenum:post.currPage});
+							});
+						}
+						else{
+							post.addPost($scope.post.title, $scope.post.body)
+							.then(function(){
+								$state.go('userhome', {userid:post.user._id, pagenum:post.currPage});
+							});
+						}
 					};
 					
 					$scope.cancel = function(){
-						$state.go('userhome', {userid:post.obj.userid});
+						$state.go('userhome', {userid:post.user._id, pagenum:post.currPage});
 					}
 				}]);
