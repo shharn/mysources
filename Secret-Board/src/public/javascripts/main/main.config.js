33537a6f83b529a1b3504cd0e83af4598c7486dc
shharn <shharn8389@naver.com>
2016-07-07 21:25:46 +0900

    Update main.config.js

--- Secret-Board/src/public/javascripts/main/main.config.js
+++ Secret-Board/src/public/javascripts/main/main.config.js
@@ -15,17 +15,27 @@ angular
 							controller: 'registerController',
 						})
 						.state('userhome', {
-							url: '/:userid/posts',
+							url: '/:userid/posts/:pagenum',
 							templateUrl: '/views/posts-list.html',
 							controller: 'postsListController',
 							resolve: {
-								userPromise: ['$stateParams', 'post', function($stateParams, post){
-									return post.getPosts($stateParams.userid);
-								}]
-							}
+									userPromise: ['$stateParams', 'post', function($stateParams, post){
+										return post.getPosts($stateParams.userid, $stateParams.pagenum);
+									}]
+							},
 						})
 						.state('newpost', {
 							url: '/:userid/posts/newpost/',
+							templateUrl: '/views/new-post.html',
+							controller: 'newpostController'
+						})
+						.state('post', {
+							url: '/:userid/post/:pagenum-:postIndex',
+							templateUrl: '/views/post.html',
+							controller: 'postController'
+						})
+						.state('editpost', {
+							url:'/:userid/post/:pagenum-:postIndex/edit',
 							templateUrl: '/views/new-post.html',
 							controller: 'newpostController'
 						});
