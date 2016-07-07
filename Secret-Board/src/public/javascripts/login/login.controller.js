62bce8a531609d8abe527d8d22164db85d37f1f5
shharn <shharn8389@naver.com>
2016-07-07 21:28:01 +0900

    Update login.controller.js

--- Secret-Board/src/public/javascripts/login/login.controller.js
+++ Secret-Board/src/public/javascripts/login/login.controller.js
@@ -3,12 +3,13 @@ angular
 		.controller('loginController', 
 				['$scope', '$http', '$state', 'auth', 
 				 function($scope, $http, $state, auth){
-			$scope.user = {};
-			$scope.logIn = function(){
-				auth.logIn($scope.user).error(function(err){
-					$scope.error = err;
-				}).then(function(){
-					$state.go('userhome', {userid: auth.id});
-				});			
-			};
+					$scope.height = window.innerHeight;
+					$scope.user = {};
+					$scope.logIn = function(){
+						auth.logIn($scope.user).error(function(err){
+							$scope.error = err;
+						}).then(function(){
+							$state.go('userhome', {userid: auth.id, pagenum:1});
+						});			
+				};
 		}]);
