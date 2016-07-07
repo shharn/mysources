b2eae709c37c5db078580ff375478850cd552eb3
shharn <shharn8389@naver.com>
2016-07-07 21:31:20 +0900

    Update post.factory.js

--- Secret-Board/src/public/javascripts/post/post.factory.js
+++ Secret-Board/src/public/javascripts/post/post.factory.js
@@ -5,13 +5,30 @@ angular
 					user : {},
 			};
 			
-			obj.getPosts = function(userid){
-				return $http.get('/' + userid + '/posts', 
+			obj.getPosts = function(userid, pagenum){
+				return $http.get('/' + userid + '/posts/' + pagenum, 
 						{headers: {Authorization: 'Bearer ' + auth.getToken()}})
 						.success(function(data){
 							angular.copy(data, obj.user);
-				});
-			};
+							var arr = [];
+							var pageStart= (pagenum - 1) - (pagenum - 1) % 10 + 1;
+							var endPage;
+							
+							if(data.numberPosts / 10 >= pageStart + 9 )
+								endPage = pageStart + 9;
+							else{
+								endPage = data.numberPosts / 10;
+								if(data.numberPosts % 10 > 0)
+									endPage++;
+							}
+												
+							for(var idx = pageStart; idx <= endPage; idx++)
+								arr.push(idx);
+							
+							obj.pageArr = arr;
+							obj.currPage = pagenum;
+						});
+				};
 			
 			obj.addPost = function(title, body){
 				var data = {};
@@ -25,5 +42,17 @@ angular
 						});
 			};
 			
+			obj.deletePost = function(postIndex){
+				return $http.post('/' + obj.user._id + '/post/' + obj.user.posts[postIndex]._id +
+						'/delete', null, {headers: {Authorization : 'Bearer ' + auth.getToken()}});
+			};
+			
+			obj.editPost = function(title, body, postId){
+				var data = {};
+				data.title = title;
+				data.body = body;
+				return $http.post('/' + obj.user._id + '/post/' + postId +
+						'/edit', data, {headers: {Authorization : 'Bearer ' + auth.getToken()}});
+			}
 			return obj;
 		}]);
