ff6c0b2297fc88f8edeb28baa7ea82fd1d079987
shharn <shharn8389@naver.com>
2016-09-19 23:11:21 +0900

    Update Robot Rock Band Small.cpp

--- Algorithm-Problem/Google-Codejam/Robot-Rock-Band/Robot Rock Band Small.cpp
+++ Algorithm-Problem/Google-Codejam/Robot-Rock-Band/Robot Rock Band Small.cpp
@@ -1,10 +1,36 @@
 // problem at : https://code.google.com/codejam/contest/5254486/dashboard#s=p1
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 #define INPUT_PATH "B-small-practice.in"
 #define OUTPUT_PATH "B-small-out.out"
 #define fori0(i, s, e) for(int (i)=(s); (i)<(e); (i)++)
 #define fori1(i, s, e) for(int (i)=(s); (i)<=(e); (i)++)
+
+mll members[4];
+int N;
+ll K;
+
+void readInput() {
+	std::cin >> N >> K;
+	fori0(i, 0, 4) fori0 (j, 0, N) {
+		ll tmp;
+		std::cin >> tmp;
+		if (members[i][tmp])
+			members[i][tmp]++;
+		else members[i][tmp] = 1;
+	}
+}
+
+void solve(int ct) {
+	ll ans = 0;
+	forit(mll, it1, members[0]) forit(mll, it2, members[1]) forit(mll, it3, members[2]) forit(mll, it4, members[3]){
+		if ((it1->first ^ it2->first ^ it3->first ^ it4->first) == K)
+			ans += it1->second * it2->second * it3->second * it4->second;
+	}
+	std::cout << "Case #" << ct << ": " << ans << std::endl;
+	fori0(i, 0, 4)
+		members[i].clear();
+}
 
 void main() {
 	freopen(INPUT_PATH, "r", stdin);
@@ -12,24 +38,11 @@ void main() {
 	int T;
 	std::cin >> T;
 	fori1(ct, 1, T) {
-		int N, K, ans = 0;
+		int ans = 0;
 		std::vector<int> members[4];
 		std::cin >> N >> K;
-		fori0(i, 0, 4) {
-			fori0(j, 0, N) {
-				int tmp;
-				std::cin >> tmp;
-				members[i].push_back(tmp);
-			}
-		}
-
-		fori0(i, 0, N) fori0(j, 0, N) fori0(k, 0, N) fori0(l, 0, N) {
-			if ((members[0][i] ^ members[1][j] ^ members[2][k] ^ members[3][l]) == K)
-				ans++;
-		}
-		std::cout << "Case #" << ct << ": " << ans << std::endl;
-		fori0(i, 0, 4)
-			members[i].clear();
+		readInput();
+		solve(ct);
 	}
 	_fcloseall();
 }
