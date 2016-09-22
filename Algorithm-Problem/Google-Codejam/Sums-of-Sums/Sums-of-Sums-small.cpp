8715a73fe33fba980b4b47ae91577862dd7c500f
shharn <shharn8389@naver.com>
2016-09-22 12:27:08 +0900

    Update Sums-of-Sums-small.cpp

--- Algorithm-Problem/Google-Codejam/Sums-of-Sums/Sums-of-Sums-small.cpp
+++ Algorithm-Problem/Google-Codejam/Sums-of-Sums/Sums-of-Sums-small.cpp
@@ -5,10 +5,16 @@
 #define SMALL_OUTPUT_PATH "D-small-out.out"
 #define fori0(i, s, e) for(int (i)=(s); (i)<(e); (i)++)
 #define fori1(i, s, e) for(int (i)=(s); (i)<=(e); (i)++)
-typedef std::vector<int> vi;
 typedef long long ll;
+typedef std::vector<ll> vll;
 int org[1000];
 vi list;
+
+void partialSum() {
+	fori0(i, 1, list.size())
+		list[i] += list[i - 1];
+}
+
 void solve(int N, int Q, int ct) {
 	std::cout << "Case #" << ct << ":\n";
 	memset(org, 0, sizeof(org));
@@ -20,12 +26,12 @@ void solve(int N, int Q, int ct) {
 			list.push_back(list.back() + org[j]);
 	}
 	std::sort(list.begin(), list.end());
+	partialSum();
 	fori0(i, 0, Q) {
 		int l, r;
 		ll ans = 0;
 		std::cin >> l >> r;
-		fori1(j, l - 1, r - 1) 
-			ans += list[j];
+		ans = list[r - 1] - list[l - 1];
 		std::cout << ans << std::endl;
 	}
 }
