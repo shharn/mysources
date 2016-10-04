6cbca7ecc91de153c095b7f97509e3c4faca0239
shharn <shharn8389@naver.com>
2016-10-04 21:16:44 +0900

    Update LastDigit.cpp

--- Algorithm-Problem/Topcoder/LastDigit/LastDigit.cpp
+++ Algorithm-Problem/Topcoder/LastDigit/LastDigit.cpp
@@ -12,42 +12,23 @@ typedef long long ll;
 class LastDigit {
 public:
 	ll calc(ll n) {
-		ll s = 0, factor = 1, d;
-		int size = 0;
-		while (true) {
-			if (n / factor != 0) {
-				size++;
-				factor *= 10;
-			}
-			else break;
-		}
-		size--;
-		factor /= 10;
-		ll memo = factor;
-		fori1(i, 0, size) {
-			ll nn = n;
-			ll a = memo;
-			ll local = 0;
-			fori1(j, 0, i) {
-				local += nn / a;
-				nn = nn % a;
-				a /= 10;
-			}
-			s += local*factor;
-			factor /= 10;
+		ll power = 1, s = 0;
+		fori0(i, 0, 18) {
+			s += n / power;
+			power *= 10;
 		}
 		return s;
 	}
 
 	ll findX(ll S) {
-		ll low = 0, high = 1e18;
+		ll low = 0, high = S;
 		while (low < high) {
-			ll mid = (low + high) / 2;
+			ll mid = (low + high) >> 1;
 			ll t = calc(mid);
 			if (t > S)
-				high = mid;
+				high = mid-1;
 			else if (t < S)
-				low = mid;
+				low = mid+1;
 			else
 				return mid;
 		}
